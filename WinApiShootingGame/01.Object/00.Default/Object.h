5aeb2ae38d0658f57a24ee69f80b1482c7de80fc
pgu1213 <pgu5136@gmail.com>
2025-05-10 23:00:23 +0900

    [FIX]

--- WinApiShootingGame/01.Object/00.Default/Object.h
+++ WinApiShootingGame/01.Object/00.Default/Object.h
@@ -14,8 +14,8 @@ public:
     bool IsValid() const; // 객체가 유효한지 확인
 
 private:
-    static atomic<unsigned int> NextObjectID; // 임시 UID
+    atomic<unsigned int> NextObjectID; // 임시 UID
     unsigned int ObjectID; // 이 객체의 고유 ID
-    std::string ObjectName;     // 객체의 이름 (디버깅 또는 식별 목적)
+    string ObjectName;     // 객체의 이름 (디버깅 또는 식별 목적)
     bool bIsValid; // 객체가 유효한 상태인지 나타내는 플래그 (가비지 컬렉션 유사 개념 시 활용)
 };
